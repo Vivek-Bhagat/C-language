413e5c7987458a373ff4be98cd9851b0676718bf
Vivek <vivekbhagat1890@hotmail.com>
2023-05-08 12:21:35 +0530

    escape character

---
+++ escapecharacter.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    printf("\n below is a classic example");
+    printf(" \n \f \f ");
+
+    printf("\n usgae of vertical tab escape sequence");
+    printf(" \n \v vignesh \t krishnakumar ");
+
+    printf("\n learning the null value ");
+    printf(" \n ooculussss \000 reparo bnmhgfh");
+
+    printf("\n below is the demo of  printing octal value");
+    char *b = "B\126";
+    printf(" \n%s", b);
+
+    return 0;
+}
