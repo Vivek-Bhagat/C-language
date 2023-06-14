c0a2bedce432b6825f032aaaed91031aedb8c6c2
Vivek <vivekbhagat1890@hotmail.com>
2023-06-15 02:30:13 +0530

    Tail recursion

---
+++ TailRecursion.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void fun();
+
+void fun( int n)
+{
+    if(n==0)
+    return;
+    else
+    {
+        printf("%d",n);
+        return fun(n-1);
+    }
+}
+
+int main()
+{
+    fun(3);
+    return 0;
+}
