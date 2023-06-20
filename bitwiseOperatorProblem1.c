556a8acf1505067069e3bb89aa4ff008c728a5e2
Vivek <vivekbhagat1890@hotmail.com>
2023-06-20 21:34:02 +0530

    bitwise hacker rank problem 1 solution

---
+++ bitwiseOperatorProblem1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+// Complete the following function.
+
+void calculate_the_maximum(int n, int k)
+{
+    // Write your code here.
+    int max_and =0, max_or=0, max_xor=0;
+    for(int i=1; i<=n;i++)
+    {
+        for(int j=i+1; j<=n; j++)
+        {
+            int AND = i&j;
+            int OR = i|j;
+            int XOR = i^j;
+
+            if( AND < k && AND > max_and)
+            max_and = AND;
+
+            if( OR < k && OR > max_or)
+            max_or = OR;
+
+            if( XOR < k && XOR > max_xor)
+            max_xor = XOR;
+        }
+    }
+    printf("%d %d %d", max_and, max_or, max_xor);
+}
+
+int main()
+{
+    int n, k;
+
+    scanf("%d %d", &n, &k);
+    calculate_the_maximum(n, k);
+
+    return 0;
+}
--- bitwiseoperators.c
+++ bitwiseoperators.c
@@ -3,9 +3,9 @@ main()
 {
     int a = 20, b = 40;
     // Binary: a=10100 and b=101000
-    printf("\na&b = %d", a & b);
-    printf("\na|b = %d", a | b);
-    printf("\na^b = %d", a ^ b);
+    printf("\na&b = %d", a & b);     //*And Operator
+    printf("\na|b = %d", a | b);   //* OR operator
+    printf("\na^b = %d", a ^ b);      //*  XOR operator
     printf("\n~a = %d", ~a);
     printf("\na<<2 = %d", a << 2);
     printf("\na>>2 = %d", a >> 2);
